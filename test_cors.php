90ffd36564b840f2add2f3e815ab9bde19bf69dc
CalVinDiesel <chiazsc-sm22@student.tarc.edu.my>
2026-04-07 12:12:33 +0800

    delete unused file

--- test_cors.php
+++
@@ -1,7 +0,0 @@
-<?php
-$ch = curl_init('https://3dhub.geosabah.my/3dmodel/KK_OSPREY/tileset.json');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HEADER, true);
-curl_setopt($ch, CURLOPT_NOBODY, true);
-$result = curl_exec($ch);
-echo $result;
